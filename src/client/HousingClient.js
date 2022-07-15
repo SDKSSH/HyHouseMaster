3bdb69978872f1b627d836289ae3579ba327f03b
SDKSSH <sdk_ssh@protonmail.com>
2022-07-15 19:43:47 +0200

    Switch to typescript

--- src/client/HousingClient.js
+++
@@ -1,6 +0,0 @@
-const mineFlayer = require("mineflayer");
-class Client {
-    constructor(options = {username, password, version : "1.17"}){
-        this.options = options;
-    }
-}
---
+++ src/client/HousingClient.ts
@@ -0,0 +1,18 @@
+const mineFlayer = require("mineflayer");
+/**class Client {
+    constructor(options = {username, password, version : "1.17.0"}){
+        this.options = options;
+    }
+
+    start(housing = {username, houseName}) {
+        mineFlayer.createBot({
+            username: this.options.username,
+            password: this.options.password,
+            version: this.options.version,
+            host: "play.hypixel.net",
+            port: 25565,
+            auth: "microsoft",
+
+        })
+    }
+}**/
