2b48310dcd567945fc4898a118cea5dba28e399f
SDKSSH <sdk_ssh@protonmail.com>
2022-07-16 13:46:43 +0200

    Create MineFlayer Client

--- src/client/HousingClient.ts
+++ src/client/HousingClient.ts
@@ -1,11 +1,33 @@
 import { ClientOptions } from "../options/ClientOptions";
-const mineFlayer = require("mineflayer")
-
+import * as mineFlayer from "mineflayer"
 
 export class HousingClient {
     private options: ClientOptions;
+    private mineclient : mineFlayer.Bot; 
 
     constructor(options: ClientOptions) {
         this.options = options;    
     }
+
+    getOptions(){
+        return this.options;
+    }
+
+    getMineClient(){
+        return this.mineclient;
+    }
+
+    login(){
+        this.mineclient = mineFlayer.createBot(
+            {
+                "host" : "play.hypixel.net",
+                "port" : 25565,
+                "auth" : "microsoft",
+                "version" : this.options.version,
+                "username" : this.options.account.username,
+                "password" : this.options.account.password
+            }
+        )Z
+    }
+     
 }
